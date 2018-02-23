6d9e5151e98b8e7c6ce840a12a1423950c5d4eea
Filipp Bakanov <filipp@bakanov.su>
2018-02-23 21:37:57 +0300

    geometry extraction

--- solve.py
+++ solve.py
@@ -18,13 +18,49 @@ for i in xrange(0,h-1):
         c = edges[i, j]
         edges[i, j] = max(l, d, c)
 
-backtorgb = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
+#backtorgb = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
 
-h, w = backtorgb.shape[:2]
+h, w = edges.shape[:2]
 mask = np.zeros((h+2, w+2), np.uint8)
-cv2.floodFill(backtorgb, mask, (0,0), 255)
+cv2.floodFill(edges, mask, (0,0), 42)
 
-cv2.imshow('edges', backtorgb)
+for i in xrange(0,h-1):
+    for j in xrange(0,w-1):
+        c = edges[i, j]
+        if c != 42:
+            edges[i, j] = 255
+        else:
+            edges[i, j] = 0
+
+#edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
+_, contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+print ' === total:', len(contours)
+
+#cv2.drawContours(img, contours, -1, (0,255,0), 3)
+
+for cnt in contours:
+    area = cv2.contourArea(cnt)
+    if area < 1000:
+        continue
+    print ' === cnt:'
+    print cnt
+    cv2.drawContours(img, [cnt], 0, (255, 0, 0), 3)
+    # if area < 2000 or area > 4000:
+    #     continue
+
+    # if len(cnt) < 5:
+    #     continue
+
+    #ellipse = cv2.fitEllipse(cnt)
+    #cv2.ellipse(img, ellipse, (0,255,0), 2)
+
+    # epsilon = 0.1 * cv2.arcLength(cnt, True)
+    # approx = cv2.approxPolyDP(cnt, epsilon, True)
+    # print ' === approx:'
+    # print approx
+
+cv2.imshow('edges', img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
